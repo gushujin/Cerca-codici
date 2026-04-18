bd3600a85ad809a0b4683ab4149c2245cb456b30
gushujin <gushujin@gmail.com>
2026-04-18 20:47:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ st.markdown("""
 
 # --- SIDEBAR: IMPOSTAZIONI DI SISTEMA ---
 st.sidebar.title("🛠️ Inserimento Parametri ")
-st.sidebar.image("https://upload.wikimedia.org/wikipedia/commons/5/5f/Siemens-logo.svg", width=1500)
+st.sidebar.image("https://upload.wikimedia.org/wikipedia/commons/5/5f/Siemens-logo.svg", width=150)
 st.sidebar.title("Configuratore Multibrand")
-brand = st.sidebar.selectbox("Brand Selezionato", ["SIEMENS (Original)", "ABB (Equivalent)", "Schneider (Equivalent)"])
+brand = st.sidebar.selectbox("Seleziona il Brand", ["SIEMENS (Original)", "ABB (Equivalent)", "Schneider (Equivalent)"])
 st.sidebar.divider()
 st.sidebar.markdown("### Supporto Tecnico\nBasato su Catalogo SENTRON 10/2019")
 
