2bd74ec576a22daa0a5e1e0f2ce11617f07d0695
gushujin <gushujin@gmail.com>
2026-04-23 23:11:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -72,6 +72,16 @@ with col_params:
         with c1:
             fam_code = st.selectbox("Famiglia (POS.1-2)", ["A9", "R9"])
             serie_code = "F" # Serie fissa iC60N
+            serie_map = {
+                "iC60N (Standard)": "F",
+                "iC60H (High)": "H",
+                "iC60L (Limiters)": "L",
+                "iK60": "K",
+                "DPN": "N",
+                "Resi9": "R"
+            }
+
+            
             st.text_input("Serie (POS.3)", value="F", disabled=True)
             pdi_val = st.selectbox("PDI (POS.4)", ["6 kA", "10 kA", "15 kA"])
             pdi_map = {"6 kA": "7", "10 kA": "8", "15 kA": "9"}
