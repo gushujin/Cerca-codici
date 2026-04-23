23e36f3fc6a71674e6a153b346c97078f0fb1a8c
gushujin <gushujin@gmail.com>
2026-04-23 17:20:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -60,30 +60,73 @@ with col_params:
         pos_data = [("1-2", "5S"), ("3", serie_code), ("4", p_code), ("5", pol_code), ("6-7", amp_fixed), ("8", c_code)]
         url_base = "https://support.industry.siemens.com/cs/products?search="
 
-    elif brand == "SCHNEIDER":
+  elif brand == "SCHNEIDER":
         c1, c2 = st.columns(2)
         with c1:
-            # Vincolo: Schneider iC60 parte da 6kA
-            pdi_val = st.selectbox("Potere Interruzione (PDI - POS.4)", ["6 kA", "10 kA", "15 kA"])
-            pdi_map = {"6 kA":"7", "10 kA":"8", "15 kA":"9"}
-            p_code = pdi_map.get(pdi_val)
-            # Vincoli POS.5 Curva
-            curva_val = st.selectbox("Curva (POS.5)", ["Curva B", "Curva C", "Curva D"])
-            curv_map = {"Curva B":"2", "Curva C":"3", "Curva D":"5"} # Mappatura reale iC60N
-            c_code = curv_map.get(curva_val)
+            # POS. 1-2: FAMIGLIA (Da Allegato Schneider)
+            fam_opt = {
+                "A9 = Terziario/commerciale": "A9",
+                "R9 = Residenziale": "R9"
+            }
+            fam_sel = st.selectbox("Famiglia (POS.1-2)", list(fam_opt.keys()))
+            fam_code = fam_opt[fam_sel]
+
+            # POS. 3: SERIE (Vincolo: F)
+            serie_opt = {"F = iC60N, 6 kA": "F"}
+            serie_sel = st.selectbox("Serie (POS.3)", list(serie_opt.keys()))
+            serie_code = serie_opt[serie_sel]
+
+            # POS. 4: PDI (Da Allegato Schneider)
+            pdi_opt = {
+                "7 = Ic60n-6.000 A (6 kA)": "7",
+                "8 = iC60H-10.000 A (10 kA)": "8",
+                "9 = iC60L-15.000 A (15 kA)": "9"
+            }
+            pdi_sel = st.selectbox("PDI (POS.4)", list(pdi_opt.keys()))
+            p_code = pdi_opt[pdi_sel]
+
+            # POS. 5: CURVA (Da Allegato Schneider)
+            curv_opt = {
+                "3 = Curva B": "3",
+                "4 = Curva C": "4",
+                "2 = Curva D": "2"
+            }
+            curv_sel = st.selectbox("Curva (POS.5)", list(curv_opt.keys()))
+            c_code = curv_opt[curv_sel]
+
         with c2:
-            # Vincoli POS.6 Poli
-            poli_val = st.selectbox("Poli (POS.6)", ["1P", "2P", "3P", "4P", "1P+N"])
-            pol_map = {"1P":"1", "2P":"2", "3P":"3", "4P":"4", "1P+N":"5"}
-            pol_code = pol_map.get(poli_val)
-            # Vincoli POS.7-8
-            amp_val = st.selectbox("Corrente (In - POS.7-8)", ["6A", "10A", "16A", "20A", "25A", "32A", "40A", "50A", "63A"])
-            amp_fixed = amp_val.replace("A", "").zfill(2)
+            # POS. 6: POLI (Da Allegato Schneider)
+            pol_opt = {
+                "1 = 1P": "1",
+                "2 = 2P": "2",
+                "3 = 3P": "3",
+                "4 = 4P": "4",
+                "5 = 1P+N": "5"
+            }
+            pol_sel = st.selectbox("Poli (POS.6)", list(pol_opt.keys()))
+            pol_code = pol_opt[pol_sel]
 
-        codice_final = f"A9F{p_code}{c_code}{pol_code}{amp_fixed}"
-        pos_data = [("1-2", "A9"), ("3", "F"), ("4", p_code), ("5", c_code), ("6", pol_code), ("7-8", amp_fixed)]
+            # POS. 7-8: CORRENTE NOMINALE (Da Allegato Schneider)
+            amp_opt = [
+                "06 = 6A", "10 = 10A", "16 = 16A", "20 = 20A", 
+                "25 = 25A", "32 = 32A", "40 = 40A", "50 = 50A", "63 = 63A"
+            ]
+            amp_sel = st.selectbox("Corrente Nominale (POS.7-8)", amp_opt)
+            amp_fixed = amp_sel.split(" ")[0] # Estrae '06', '10', ecc.
+
+        # Composizione finale secondo la sequenza corretta
+        codice_final = f"{fam_code}{serie_code}{p_code}{c_code}{pol_code}{amp_fixed}"
+        
+        # Struttura per il Box Analisi
+        pos_data = [
+            ("1-2", fam_code), 
+            ("3", serie_code), 
+            ("4", p_code), 
+            ("5", c_code), 
+            ("6", pol_code), 
+            ("7-8", amp_fixed)
+        ]
         url_base = "https://www.se.com/it/it/search/"
-
     elif brand == "HAGER":
         c1, c2 = st.columns(2)
         with c1:
